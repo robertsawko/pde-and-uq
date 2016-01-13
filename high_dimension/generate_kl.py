bfaefe8cf9af691d476db5419fe4d51f588cded7
Robert Sawko <robertsawko@gmail.com>
2016-01-13 13:16:46 +0000

    Increase the KL resolution

--- high_dimension/generate_kl.py
+++ high_dimension/generate_kl.py
@@ -1,6 +1,5 @@
 from numpy import logspace, linspace, pi, sin
 from numpy.random import randn
-# from kl_expansion import kl_covariance
 from kl_expansion import kl_expansion_fourier_projected
 import pickle
 from matplotlib.pyplot import figure
@@ -8,14 +7,15 @@ from scipy.signal import welch
 from generate_shinozuka import ftransform_analytical
 
 
-m = 2000
-eta = randn(m)
+M = 2000
+eta = randn(M)
 eta0 = randn(1)
-a = pi
-N = 4000
+L = 2 * pi
+N = 20000
 b = 100
-x = linspace(-a, a, N)
-kl = kl_expansion_fourier_projected(x, m=m, n=20, a=a, c=b, eta=eta)
+x = linspace(0, L, N)
+kl = kl_expansion_fourier_projected(
+    x, no_of_kl_modes=M, no_of_fourier_modes=M, a=L / 2, c=b, eta=eta)
 y3 = 0.5 * (1.0 + eta0 + sin(x)) + kl
 
 with open('u0.pickle', 'wb') as f:
@@ -31,7 +31,7 @@ f, pxx = welch(kl, fs=1 / (x[1] - x[0]), nperseg=N // 10)
 axc = figc.gca()
 axc.loglog(
     2 * pi * f, pxx / 4 / pi,
-    label='Empirical m={0:n}'.format(m))
+    label='Empirical M={0:n}'.format(M))
 omega = logspace(-1, 4, 10000)
 a = ftransform_analytical(omega, b=b)
 axc.loglog(omega, a, label='Analytical')
